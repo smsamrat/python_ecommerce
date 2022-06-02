76d13bd1cd2e1d6f598b688c09e5355ef3d0fa61
you <97931972+smsamrat@users.noreply.github.com>
2022-06-02 11:40:37 +0600

    menu and product show on template

--- App_store/models.py
+++ App_store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Category (models.Model):
+    title = models.CharField(max_length=250,unique=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
+    class Meta:
+        verbose_name_plural = "Categories"
+
+class Product (models.Model):
+    mainimage = models.ImageField(upload_to = "Products")
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=264, unique=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    preview_text = models.TextField(max_length=250, verbose_name="preview_text")
+    details_text = models.TextField(max_length=250,verbose_name="description")
+    price = models.FloatField()
+    old_price = models.FloatField(default=0.00)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
+    class Meta:
+        ordering =['-created']
--- App_store/views.py
+++ App_store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Product,Category
 
 # Create your views here.
 
 def store(request):
-    return render(request,'app_store/store.html')
+    products = Product.objects.all()
+    context ={
+         'object_list': products
+    }
+    return render(request,'app_store/store.html',context)
