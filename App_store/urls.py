26c37e4b92522258f833738e8004b9e988a9cbf5
you <97931972+smsamrat@users.noreply.github.com>
2022-06-02 12:09:03 +0600

    changed

--- App_store/urls.py
+++ App_store/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.store, name='store'),
+    path('cetegory/<str:slug>/',views.product_fetch_by_category, name='category'),
+    path('product-details/<str:cat_slug>/<str:prod_slug>/', views.product_details, name='product_details'),
 ]
--- App_store/views.py
+++ App_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product,Category
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,3 +10,17 @@ def store(request):
          'object_list': products
     }
     return render(request,'app_store/store.html',context)
+
+def product_fetch_by_category(request, slug):
+    if (Category.objects.filter(slug = slug)):
+        products = Product.objects.filter(category__slug = slug)
+        return render(request,'app_store/store.html',context={'object_list':products})
+    else:
+        messages.warning(request,'Product is not available')
+        return redirect('store')
+
+def product_details(request,cat_slug,prod_slug):
+    if(Category.objects.filter(slug=cat_slug)):
+        if(Product.objects.filter(slug=prod_slug)):
+            products = Product.objects.filter(slug=prod_slug).first()
+    return render(request,'app_store/product_details.html',context={'single_product':products})
