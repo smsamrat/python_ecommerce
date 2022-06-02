32e305cbb9eb8527861e6ee2d2b4fb8974162ba4
you <97931972+smsamrat@users.noreply.github.com>
2022-06-02 23:52:55 +0600

    product details page

--- App_store/urls.py
+++ App_store/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('category/<str:slug>/', views.categoryfetchItem, name='category'),
+    path('category/<str:cat_slug>/<str:prod_slug>/', views.categoryfetchItem, name='product_details'),
 
 ]
--- App_store/views.py
+++ App_store/views.py
@@ -24,3 +24,14 @@ def categoryfetchItem(request,slug):
     else:
         messages.warning(request,'Product is not available')
         return render(request,'app_store/store.html')
+
+        
+def categoryfetchItem(request,cat_slug,prod_slug):
+
+    if(Category.objects.filter(slug=cat_slug)):
+        if (Product.objects.filter(slug=prod_slug)):
+            products = Product.objects.filter(slug=prod_slug).first()
+            context = {
+                'single_product': products
+            }
+        return render(request,'app_store/product_details.html',context)
